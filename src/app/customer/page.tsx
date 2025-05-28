09ba680f5b8938a14ad8354bdb553f0791ff3288
Izhar <izharramdan93@gmail.com>
2025-05-28 10:03:08 +0700

    update dependecies

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,11 +1,14 @@
-// src/app/page.tsx
-import CustomerTable from "@/components/CustomerTable";
+// app/customer/page.tsx
+import { Suspense } from 'react';
+import CustomerTable from '@/components/CustomerTable';
 
-export default function HomePage() {
+export default function CustomerPage() {
   return (
-    <main>
+    <main className="p-6">
       <h1 className="text-2xl font-bold text-gray-800 mb-4">Customer</h1>
-      <CustomerTable />
+      <Suspense fallback={<div>Loading customer table...</div>}>
+        <CustomerTable />
+      </Suspense>
     </main>
   );
 }
--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import SummaryTabs from "@/components/SummaryTabs";
 
 export default function SummaryPage() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-gray-800 mb-4">Summary</h1>
-      <SummaryTabs />
+      <Suspense fallback={<div>Loading data...</div>}>
+        <SummaryTabs />
+      </Suspense>
     </div>
   );
 }
